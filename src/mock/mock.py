ef587eabf5f8e58c97d1985153e060993054892c
ss249tp <ss249.tp@gmail.com>
2025-01-25 23:31:03 +0800

    feat: Add `load_text()` and `tokenize()`

---
+++ src/mock/mock.py
@@ -0,0 +1,44 @@
+__all__ = (
+    "load_text",
+    "tokenize",
+)
+
+
+import os
+import string
+
+PUNCTUTATIONS = frozenset(string.punctuation)
+
+
+def load_text(path: str) -> str:
+    """Load contents of a text file into a string.
+
+    Args:
+        path: Path to a text file.
+
+    Returns:
+        Text in the file as a string.
+
+    Raises:
+        ValueError: If `path` cannot be read as a text file.
+    """
+    if not os.path.isfile(path):
+        raise ValueError(f"{path} either does not exist or is not a file.")
+
+    try:
+        with open(path, "r") as file:
+            return file.read()
+    except UnicodeDecodeError as err:
+        raise ValueError(f"Failed to read {path} as a text file.") from err
+
+
+def tokenize(text: str) -> list[str]:
+    """Split a string into a list of words.
+
+    Args:
+        text: Text string to tokenize.
+
+    Returns:
+        A list of words in `text`, but with punctuations removed and letters lowercased.
+    """
+    return "".join([(" " if c in PUNCTUTATIONS else c) for c in text.lower()]).split()
